ebc61d6186de028556e9e801825455212ca049f3
miguelsoaresmaciel68-alt <miguelsoaresmaciel68@gmail.com>
2026-03-03 20:50:41 -0300

    03/03
    03/03

---
+++ NexusGamerPro/js/script.js
@@ -0,0 +1,48 @@
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+function saveCart() {
+    localStorage.setItem("cart", JSON.stringify(cart));
+}
+
+function addToCart(name, price) {
+    cart.push({name, price});
+    saveCart();
+    updateCart();
+}
+
+function removeItem(index) {
+    cart.splice(index, 1);
+    saveCart();
+    updateCart();
+}
+
+function updateCart() {
+    document.getElementById("cart-count").innerText = cart.length;
+    let cartItems = document.getElementById("cart-items");
+    let total = 0;
+    cartItems.innerHTML = "";
+
+    cart.forEach((item, index) => {
+        let li = document.createElement("li");
+        li.innerHTML = `${item.name} - R$ ${item.price.toFixed(2)} 
+        <button onclick="removeItem(${index})">X</button>`;
+        cartItems.appendChild(li);
+        total += item.price;
+    });
+
+    document.getElementById("cart-total").innerText = total.toFixed(2);
+}
+
+function openCart() {
+    document.getElementById("cart-modal").style.display = "flex";
+}
+
+function closeCart() {
+    document.getElementById("cart-modal").style.display = "none";
+}
+
+function scrollToProducts() {
+    document.getElementById("products").scrollIntoView({ behavior: "smooth" });
+}
+
+updateCart();
